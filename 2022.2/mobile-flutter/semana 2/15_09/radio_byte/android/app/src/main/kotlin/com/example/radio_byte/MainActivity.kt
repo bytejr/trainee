8070d2cf683f91512b186f8016420027d75e6604
Eduardo Augusto <eduardo.evangelista2002@gmail.com>
2022-09-05 16:41:09 -0300

    radio_byte

---
+++ 2022.2/mobile-flutter/semana 2/15_09/radio_byte/android/app/src/main/kotlin/com/example/radio_byte/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.radio_byte
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 2022.2/mobile-flutter/semana 2/15_09/radio_byte/lib/shared/themes/app_text_style.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:radio_byte/shared/themes/app_colors.dart';
+
+class AppTextStyle {
+  static final headerTitle =
+      GoogleFonts.lato(color: AppColors.bege, fontSize: 22);
+  static final footerDescription =
+      GoogleFonts.lato(fontSize: 16, color: AppColors.bege);
+  static final sliderValue =
+      GoogleFonts.lato(fontSize: 32, fontWeight: FontWeight.w400);
+  static final fm = GoogleFonts.lato(fontSize: 32, color: AppColors.bege);
+}
