2e86a19252d4e1efc3ce732b90ecdbe20249b2aa
Eduardo Augusto <eduardo.evangelista2002@gmail.com>
2022-09-07 13:58:55 -0300

    finalizando sign_up

---
+++ 2022.2/mobile-flutter/finance_app_byte/lib/shared/themes/app_images.dart
@@ -0,0 +1,5 @@
+class AppImages {
+  static const happyIcon = "assets/happyIcon.png";
+  static const byteIcon = "assets/byteIcon.png";
+  static const moneyLogo = "assets/moneyLogo.png";
+}
---
+++ 2022.2/mobile-flutter/finance_app_byte/lib/shared/widgets/Dotted Divider/dotted_divider_header.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import '../../themes/app_colors.dart';
+
+class DottedDividerHeader extends StatelessWidget {
+  const DottedDividerHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      const Spacer(),
+      Expanded(
+        flex: 65,
+        child: Container(
+          height: 3,
+          decoration: BoxDecoration(
+            color: AppColors.blue1,
+            borderRadius: BorderRadius.circular(3),
+          ),
+        ),
+      ),
+      const Spacer(),
+      Expanded(
+        flex: 6,
+        child: Container(
+          height: 3,
+          decoration: BoxDecoration(
+            color: AppColors.blue1,
+            borderRadius: BorderRadius.circular(3),
+          ),
+        ),
+      ),
+      const Spacer(),
+      Expanded(
+        flex: 2,
+        child: Container(
+          height: 3,
+          decoration: BoxDecoration(
+            color: AppColors.blue1,
+            borderRadius: BorderRadius.circular(3),
+          ),
+        ),
+      ),
+    ]);
+  }
+}
