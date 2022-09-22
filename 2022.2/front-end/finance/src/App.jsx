6229c0dae0b51b0e2681bcbf8788b4d33c2ac032
joao <joao@bytejr.com>
2022-09-22 14:48:18 -0300

    Front-end until 22/09

---
+++ 2022.2/front-end/finance/src/App.jsx
@@ -0,0 +1,16 @@
+import { Header } from './components/Header'; 
+import { Footer } from './components/Footer';
+
+import './styles/global.scss';
+
+function App() {
+
+  return (
+    <div className="div">
+      <Header/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
